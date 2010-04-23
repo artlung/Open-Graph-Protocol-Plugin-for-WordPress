126217bcc3ac51dae76f5fb3aa7731b11c1ceeea
Joe Crawford <artlung@frankiemouse.local>
2010-04-22 21:45:18 -0400

    First Version of the Plugin

---
+++ open-graph-protocol-tools/open-graph-protocol-tools.php
@@ -0,0 +1,78 @@
+<?php
+/*
+Plugin Name: Open Graph Protocol Tools
+Plugin URI: http://lab.artlung.com/open-graph-protocol-tools/
+Description: Tools for Open Graph Protocol
+Version: 1
+Author: Joe Crawford
+Author URI: http://joecrawford.com
+License: GPL2
+*/
+// http://opengraphprotocol.org/
+
+/*  Copyright 2010  Joe Crawford  (email : joe@artlung.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+// opengraphprotocol.default.png
+
+define('OGPT_DEFAULT_TYPE', 'blog');
+
+function opengraphprotocoltools_add_head() {
+		// REQUIRED
+		// title ~ title of web page
+		// type ~ blog
+		// image ~ ?
+		// url ~ url of post
+		global $wp_query;
+		$data = array();
+		if (is_home()) :
+			$data['title'] = get_bloginfo('name');
+			$data['type'] = OGPT_DEFAULT_TYPE;
+			$data['image'] = get_bloginfo('stylesheet_directory')."/opengraphprotocol.default.png";
+			$data['url'] = get_bloginfo('url');
+			$data['site_name'] = get_bloginfo('name');
+			$data['description'] = '';
+		elseif (is_single() || is_page()):
+			$data['title'] = get_the_title();
+			$data['type'] = OGPT_DEFAULT_TYPE;
+			$data['image'] = get_bloginfo('stylesheet_directory')."/opengraphprotocol/default.png";
+			$data['url'] = get_permalink();
+			$data['site_name'] = get_bloginfo('name');
+			$data['description'] = '';
+		endif;
+		echo get_opengraphprotocoltools_headers($data);
+}
+
+function get_opengraphprotocoltools_headers($data) {
+	if (!count($data)) {
+		return;
+	}
+	$out = array();
+	$out[] = "\n<!-- BEGIN: Open Graph Protocol Tools: http://opengraphprotocol.org/ for more info -->";
+	foreach ($data as $property => $content) {
+		$out[] = get_opengraphprotocoltools_tag($property, $content);
+	}
+	$out[] = "<!-- End: Open Graph Protocol Tools-->\n";
+	return implode("\n", $out);
+}
+
+function get_opengraphprotocoltools_tag($property, $content) {
+	return "<meta property=\"og:{$property}\" content=\"".htmlentities($content)."\" />";
+}
+
+
+add_action('wp_head', 'opengraphprotocoltools_add_head');
+
+?>
---
+++ open-graph-protocol-tools/readme.txt
@@ -0,0 +1,51 @@
+=== Plugin Name ===
+Contributors: artlung
+Donate link: 
+Tags: metadata, opengraphprotocol
+Requires at least: 2.9
+Tested up to: 2.9.2
+Stable tag: 1.0
+
+This is a tool to add Open Graph Protocol Data to your WordPress Install.
+
+== Description ==
+
+This is a tool to add [Open Graph Protocol](http://opengraphprotocol.org/) Data to your WordPress Install.
+
+This is very new at this point.
+
+The following data is a description of the metadata that at this point is just a placeholder:
+
+*   "Contributors" is a comma separated list of wp.org/wp-plugins.org usernames
+*   "Tags" is a comma separated list of tags that apply to the plugin
+*   "Requires at least" is the lowest version that the plugin will work on
+*   "Tested up to" is the highest version that you've *successfully used to test the plugin*. Note that it might work on
+higher versions... this is just the highest one you've verified.
+*   Stable tag should indicate the Subversion "tag" of the latest stable version, or "trunk," if you use `/trunk/` for
+stable.
+
+== Installation ==
+
+1. Upload `open-graph-protocol-tools/` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Is there a FAQ? =
+
+Not yet.
+
+= What about foo bar? =
+
+No, not that either.
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the directory of the stable readme.txt, so in this case, `/tags/4.3/screenshot-1.png` (or jpg, jpeg, gif)
+2. This is the second screen shot
+
+== Changelog ==
+
+= 1 =
+* First version.
