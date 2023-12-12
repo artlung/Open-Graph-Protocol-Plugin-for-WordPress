b509cf05b07c8c712f3c5c18bf548ff0e5b94a44
Joe Crawford <joe@artlung.com>
2023-12-12 06:57:50 -0800

    Initial commit after running: - wp scaffold plugin-tests open-graph-protocol-tools - and updating the resulting .dist phpunit related files

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Open_Graph_Protocol_Tools
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_first() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
